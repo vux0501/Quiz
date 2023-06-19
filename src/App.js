f48378c4b0516651c4bab3790593d8fa406ae3f9
vux0501 <hvudtkt@gmail.com>
2023-06-19 21:21:09 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header/Header';
 
 const App = () => {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        {/* <React.StrictMode> */}
-        {/* <App />
-    </React.StrictMode> */}
+        <App />
     </Provider>,
 );
 
