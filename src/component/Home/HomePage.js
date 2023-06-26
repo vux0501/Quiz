d4a7e57063eba96c020979ba45acf36da873fda4
vux0501 <hvudtkt@gmail.com>
2023-06-26 14:05:47 +0700

    done homepage

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -4,7 +4,14 @@ import Background from '../../assets/background_homepage.jpg';
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            <img className="background_homepage" src={Background} alt="Background" />
+            <img className="background-homepage" src={Background} alt="Background" />
+            <div className="homepage-content">
+                <div className="title-1">Welcome to VUX Quiz</div>
+                <div className="title-2">Knowledge is power.</div>
+                <div className="title-3">
+                    <button className="btn-title-3">Get's started</button>
+                </div>
+            </div>
         </div>
     );
 };
